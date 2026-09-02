03bac570ea0275858235a0eff17ab1619aca1b2a
Guzt-JF <cligeroficial@gmail.com>
2026-09-02 09:36:08 -0300

    feat: adicionado comentário na config mail.php

--- App/Config/mail.php
+++ App/Config/mail.php
@@ -1,8 +1,9 @@
 <?php 
+// Credenciais para conectar no servidor SMTP, substitua de acordo
 return [
   'host'       => 'smtp.gmail.com',
   'email'      => 'EMAIL',
   'password'   => 'SENHA_DE_APP',
   'port'       => 587,
-  'appname'   => 'name',
+  'appname'   => 'NOME_DO_APP',
 ];
